e704501a5852c6384a9064361c86dc8c7dbcd2e5
kiri <144Pwathy@gmail.com>
2023-03-18 14:58:17 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/home.vue'
+</script>
+
+<template>
+    <Home/>
+</template>
+<style scoped>
+
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <img class="logo" src="../assets/JNU.png" />
+  <el-text>暨南大学</el-text>
+  <el-input v-model="input" placeholder="请输入想要搜索的商品" />
+  <el-button color="#e62828" type="primary" :icon="Search">搜索</el-button>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { Delete, Edit, Search, Share, Upload } from '@element-plus/icons-vue'
+const input = ref('')
+</script>
+<style scoped>
+img {
+  width: 100px;
+  height: 100px;
+}
+</style>
